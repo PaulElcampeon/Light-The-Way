ff63a60e04b6473b15aa248ac8f6da1ef15b2194
PaulElcampeon <pauloladele@live.co.uk>
2020-04-06 21:11:24 +0100

    Modified script.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,17 +8,83 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float movementSpeed = 10f;
 
+    [Header("Physics")]
+    [SerializeField]
+    private float jumpForce = 4f;
+    [SerializeField]
+    private float passiveGravity = 17f;
+    [SerializeField]
+    private float fallingGravity = 5f;
+
+    [Header("Components")]
+    public Rigidbody2D playerRGB;
+
     private float movementDir;
+    private bool isJumping;
+    public bool isFalling;
+    public bool isDead;
+    private bool isGrounded;
+
 
     void Start()
     {
-        
+        playerRGB = GetComponent<Rigidbody2D>();
+
     }
 
     void Update()
     {
+        if (isDead) return;
+
         movementDir = Input.GetAxisRaw("Horizontal");
+
+        ListenForJumpInput();
     }
 
+    private void FixedUpdate()
+    {
+        Jump();
 
+        Movement();
+
+        Fall();
+    }
+
+    private void Jump()
+    {
+        if (isJumping && isGrounded) ActivateJump();
+    }
+
+    private void Fall()
+    {
+        if (playerRGB.velocity.y < -0.1f) ActivateFall();
+    }
+
+    private void Movement()
+    {
+        if (isDead) return;
+
+        playerRGB.velocity = new Vector2(movementDir * movementSpeed, playerRGB.velocity.y);
+    }
+
+    private void ListenForJumpInput()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded && !isDead) isJumping = true;
+    }
+
+    private void ActivateJump()
+    {
+        playerRGB.gravityScale = passiveGravity;
+        isGrounded = false;
+        isFalling = false;
+        playerRGB.velocity = Vector2.up * jumpForce;
+    }
+
+    public void ActivateFall()
+    {
+        isGrounded = false;
+        isJumping = false;
+        isFalling = true;
+        playerRGB.gravityScale = fallingGravity;
+    }
 }
