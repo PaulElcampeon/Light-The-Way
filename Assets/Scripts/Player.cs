2be4d9db09cac86087ccf5d93bd9a65d1f39a0bb
PaulElcampeon <pauloladele@live.co.uk>
2020-04-06 21:05:46 +0100

    Added new script.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Horizontal Movement")]
+    [SerializeField]
+    private float movementSpeed = 10f;
+
+    private float movementDir;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        movementDir = Input.GetAxisRaw("Horizontal");
+    }
+
+
+}
